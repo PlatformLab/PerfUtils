5a9c6061950473794338cb7fbc16f1d271a22c49
Henry Qin <hq6@cs.stanford.edu>
2018-04-28 17:08:51 -0600

    Separate rdtsc and rdtscp into distinct functions.
    TimeTrace now defaults to using rdtsc.

--- src/Cycles.h
+++ src/Cycles.h
@@ -39,6 +39,20 @@ class Cycles {
             return mockTscValue;
 #endif
         uint32_t lo, hi;
+        __asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
+        return (((uint64_t)hi << 32) | lo);
+    }
+
+    /**
+     * Return the current value of the fine-grain CPU cycle counter with
+     * partial serialization.  (accessed via the RDTSCP instruction).
+     */
+    static __inline __attribute__((always_inline)) uint64_t rdtscp() {
+#if TESTING
+        if (mockTscValue)
+            return mockTscValue;
+#endif
+        uint32_t lo, hi;
         __asm__ __volatile__("rdtscp" : "=a"(lo), "=d"(hi) : : "%rcx");
         return (((uint64_t)hi << 32) | lo);
     }
